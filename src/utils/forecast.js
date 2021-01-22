5373735ae62051502417b597f37396bbb3336d92
Eduardo Arturo Quiroz Gallegos <EduardoAQuirozG>
2021-01-21 20:56:45 -0600

    We add a console.log to know when each process starts

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,6 +5,7 @@ const weatherstack_accessKey = 'd8ad5c9bd14b3ff5ab3e5f86ac06f247';
 
 // Get the forecast
 const forecast = (latitude, longitude, callback) => {
+    console.log('We start the forecast process.')
     const weatherstack_url = 'http://api.weatherstack.com/current?access_key=' + weatherstack_accessKey + '&query=' + latitude + ',' + longitude + '&units=f'
 
     request({ 'url': weatherstack_url, 'json': true}, (error, response, body) => {
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -5,6 +5,7 @@ const mapbox_accessToken = 'pk.eyJ1IjoiZWR1YXJkb2FxdWlyb3pnIiwiYSI6ImNrazByanpqa
 
 // Get the geocode
 const geocode = (address, callback) => {
+    console.log('We start the geocode process.')
     const mapbox_url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=' + mapbox_accessToken
 
     request({ 'url': mapbox_url, 'json': true }, (error, response, body) => {
